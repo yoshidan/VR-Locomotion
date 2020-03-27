8a4f58b7288e9c8e365a099dd447eacb5b7e62f6
yoshidan <naohiro.y@gmail.com>
2020-03-27 12:45:17 +0900

    adjust ik head target

--- Assets/Scripts/IKPlayerController.cs
+++ Assets/Scripts/IKPlayerController.cs
@@ -1,6 +1,6 @@
-﻿/*
-using RootMotion.FinalIK;
+﻿using RootMotion.FinalIK;
 using UnityEngine;
+using VRM;
 
 namespace App
 {
@@ -13,16 +13,36 @@ namespace App
         public override void Start()
         {
             base.Start();
+
+            var cameraRig = FindObjectOfType<OVRCameraRig>();
             
+            //コントローラ位置を両手のIKに設定すると微妙にずれるのでトラッキングObjectを追加して調整する。
+            var leftArmTarget = new GameObject("IKLeftHand");
+            leftArmTarget.transform.SetParent(cameraRig.leftControllerAnchor);
+            leftArmTarget.transform.localPosition = new Vector3(-0.03f, 0, -0.06f);
+            leftArmTarget.transform.localRotation = Quaternion.Euler(-75, 0, 90);
+            var rightArmTarget = new GameObject("IKRightHand");
+            rightArmTarget.transform.SetParent(cameraRig.rightControllerAnchor);
+            rightArmTarget.transform.localPosition = new Vector3(0.03f, 0, -0.06f);
+            rightArmTarget.transform.localRotation = Quaternion.Euler(-75, 0, -90);
+
+            // ワープ先がアバターの目線位置なので、カメラ位置をIKのheadTargetにしてしまうとカメラが目線からずれてしまう。
+            // そのため、カメラがアバターの目線位置にくるようにトラッキング用Objectを追加する。
+            var firstPerson = GetComponent<VRMFirstPerson>();
+            var eyeCenter = firstPerson.FirstPersonOffset;
+            var headTarget = new GameObject("IKHead");
+            headTarget.transform.SetParent(cameraRig.centerEyeAnchor);
+            headTarget.transform.localPosition = -eyeCenter;
+
             _vrik = gameObject.AddComponent<VRIK>();
             _vrik.solver.plantFeet = false;
-            _vrik.solver.spine.headTarget = Camera.main.transform;
-            _vrik.solver.leftArm.target = GameObject.Find("IKLeftHand").transform;
-            _vrik.solver.rightArm.target = GameObject.Find("IKRightHand").transform;
+            _vrik.solver.spine.headTarget = headTarget.transform;
+            _vrik.solver.leftArm.target = leftArmTarget.transform;
+            _vrik.solver.rightArm.target = rightArmTarget.transform;
             _vrik.solver.locomotion.footDistance = 0.1f;
             _vrik.solver.locomotion.maxVelocity = 0.3f;
-            _vrik.solver.leftLeg.swivelOffset = 15f;
-            _vrik.solver.rightLeg.swivelOffset = -15f;    
+            _vrik.solver.leftLeg.swivelOffset = -40f;
+            _vrik.solver.rightLeg.swivelOffset = 40f;    
             _vrik.AutoDetectReferences();
             _vrik.enabled = false;
             
@@ -54,4 +74,3 @@ namespace App
         }
     }
 }
-*/
